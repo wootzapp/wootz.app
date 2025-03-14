4b28774b98cde8ea736afaadab7898543fc242cc
Sandeep Srinivasa <sss@wootzapp.com>
2025-03-14 22:26:31 +0530

    added animation

--- src/components/app/partnership.tsx
+++ src/components/app/partnership.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import useBaseUrl from '@docusaurus/useBaseUrl';
+import useBaseUrl from "@docusaurus/useBaseUrl";
 
 const data = [
 	{
@@ -17,12 +17,8 @@ const data = [
 
 export function PartnerShip() {
 	return (
-		<div className='min-h-screen relative md:pt-24 pb-16 overflow-hidden'>
-			<img
-				src={useBaseUrl('img/partnership-shadow.png')}
-				alt='Decorative shadow'
-				className='absolute -top-10 -right-16 md:-right-44 w-40'
-			/>
+		<div className='min-h-screen relative md:pt-5 pb-16 overflow-hidden'>
+			<img src={useBaseUrl("img/partnership-shadow.png")} alt='Decorative shadow' className='absolute -top-10 -right-16 md:-right-44 w-40' />
 
 			<div className='container-custom'>
 				<div className='flex flex-col gap-4 items-center text-center'>
@@ -31,9 +27,8 @@ export function PartnerShip() {
 						<span className='text-brand-orange-200 block'>Global Partnerships</span>
 					</h2>
 					<p className='text-brand-black-200 font-matter font-light max-w-4xl ~text-sm/lg'>
-						At the heart of WootzApp is our unique extension system—a first-of-its-kind solution on mobile.
-						This allows AI companies to design custom, iterative, non-standard labeling workflows, while enabling
-						developers to rapidly deploy and iterate on tasks.
+						At the heart of WootzApp is our unique extension system—a first-of-its-kind solution on mobile. This allows AI companies to design custom,
+						iterative, non-standard labeling workflows, while enabling developers to rapidly deploy and iterate on tasks.
 					</p>
 				</div>
 
@@ -41,22 +36,11 @@ export function PartnerShip() {
 					<div className='flex flex-col md:flex-row gap-6 md:gap-6 lg:gap-8 xl:gap-10'>
 						{data.map((e, index) => {
 							return (
-								<div
-									key={index}
-									className='flex bg-brand-white-100 p-5 gap-3 lg:gap-4 rounded-md basis-full z-10'
-								>
-									<img
-										src={useBaseUrl(e.img)}
-										alt={`Icon for ${e.head}`}
-										className='object-cover ~size-14/20'
-									/>
+								<div key={index} className='flex bg-brand-white-100 p-5 gap-3 lg:gap-4 rounded-md basis-full z-10'>
+									<img src={useBaseUrl(e.img)} alt={`Icon for ${e.head}`} className='object-cover ~size-14/20' />
 									<div className='flex gap-3 md:gap-4 flex-col justify-start'>
-										<h4 className='text-brand-black-100 font-matter ~text-base/xl font-semibold max-w-3xl leading-normal tracking-wide'>
-											{e.head}
-										</h4>
-										<p className='text-brand-black-200 font-matter font-med max-w-lg ~text-xs/base'>
-											{e.para}
-										</p>
+										<h4 className='text-brand-black-100 font-matter ~text-base/xl font-semibold max-w-3xl leading-normal tracking-wide'>{e.head}</h4>
+										<p className='text-brand-black-200 font-matter font-med max-w-lg ~text-xs/base'>{e.para}</p>
 									</div>
 								</div>
 							);
@@ -65,25 +49,15 @@ export function PartnerShip() {
 
 					<div className='flex flex-col-reverse md:flex-row items-center mt-5 md:mt-14 justify-center gap-5 md:gap-10 lg:gap-16'>
 						<div className='max-w-2xl flex-1'>
-							<img
-								src={useBaseUrl('img/partnership.png')}
-								alt='Partnership illustration'
-								className='w-full'
-							/>
+							<img src={useBaseUrl("img/partnership.png")} alt='Partnership illustration' className='w-full' />
 						</div>
 
 						<div className='bg-[#EEEFE9] rounded-md flex flex-1 w-full h-full p-5 gap-2 md:gap-5'>
 							<div>
-								<img
-									src={useBaseUrl('img/code.png')}
-									alt='Code icon'
-									className='w-16 md:w-24'
-								/>
+								<img src={useBaseUrl("img/code.png")} alt='Code icon' className='w-16 md:w-24' />
 							</div>
 							<div className='w-full flex flex-col gap-3'>
-								<p className='md:text-2xl font-semibold'>
-									Technical Superiority – Code Comparison
-								</p>
+								<p className='md:text-2xl font-semibold'>Technical Superiority – Code Comparison</p>
 
 								<div className='bg-[#283911] rounded-lg max-h-[796px] overflow-auto'>
 									<pre className='bg-transparent text-white p-4 whitespace-pre-wrap text-xs md:text-sm'>
--- src/components/app/performance.tsx
+++ src/components/app/performance.tsx
@@ -1,70 +1,57 @@
 "use client";
-import { performanceData } from "@site/src/data";
-import { useEffect, useState } from "react";
+import {performanceData} from "@site/src/data";
+import {useEffect, useState} from "react";
 import BrowserOnly from "@docusaurus/BrowserOnly";
 import useBaseUrl from "@docusaurus/useBaseUrl";
 
-function BaseUrlImage({ src, alt, ...props }: { src: string; alt: string; [key: string]: any }) {
-  const url = useBaseUrl(src);
-  return <img src={url} alt={alt} {...props} />;
+function BaseUrlImage({src, alt, ...props}: {src: string; alt: string; [key: string]: any}) {
+	const url = useBaseUrl(src);
+	return <img src={url} alt={alt} {...props} />;
 }
 
 export function Performance() {
-  const [windowWidth, setWindowWidth] = useState(0); // Initially 0 to avoid SSR issues
+	const [windowWidth, setWindowWidth] = useState(0); // Initially 0 to avoid SSR issues
 
-  useEffect(() => {
-    if (typeof window !== "undefined") {
-      setWindowWidth(window.innerWidth);
-      const handleResize = (e: any) => setWindowWidth(e.target.innerWidth);
-      window.addEventListener("resize", handleResize);
-      return () => window.removeEventListener("resize", handleResize);
-    }
-  }, []);
+	useEffect(() => {
+		if (typeof window !== "undefined") {
+			setWindowWidth(window.innerWidth);
+			const handleResize = (e: any) => setWindowWidth(e.target.innerWidth);
+			window.addEventListener("resize", handleResize);
+			return () => window.removeEventListener("resize", handleResize);
+		}
+	}, []);
 
-  return (
-    <BrowserOnly fallback={<div>Loading...</div>}>
-      {() => (
-        <div className="min-h-screen bg-brand-black-100 pt-24 pb-28">
-          <div className="container-custom flex flex-col justify-center gap-16">
-            <h2 className="text-brand-white-100 font-matter font-bold text-center mx-auto ~text-2xl/4xl md:max-w-3xl max-w-md">
-              Built for High <span className="text-brand-orange-200">Performance</span>,{" "}
-              <span className="text-brand-orange-300">Security</span>, and{" "}
-              <span className="text-brand-green-100">Scalability</span>
-            </h2>
+	return (
+		<BrowserOnly fallback={<div>Loading...</div>}>
+			{() => (
+				<div className='min-h-screen bg-brand-black-100 pt-24 pb-0 md:pb-28'>
+					<div className='container-custom flex flex-col justify-center gap-16'>
+						<h2 className='text-brand-white-100 font-matter font-bold text-center mx-auto ~text-2xl/4xl md:max-w-3xl max-w-md'>
+							Built for High <span className='text-brand-orange-200'>Performance</span>, <span className='text-brand-orange-300'>Security</span>, and{" "}
+							<span className='text-brand-green-100'>Scalability</span>
+						</h2>
 
-            <div className="flex gap-20 md:flex-row flex-col gap-10 items-center md:items-start">
-              <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-10 max-w-2xl mx-auto">
-                {performanceData.map((data, index) => {
-                  const res = index === 2 && windowWidth > 640 ? "col-span-2" : "";
-                  return (
-                    <div key={index} className={`flex flex-col gap-3 sm:gap-5 ${res}`}>
-                      <BaseUrlImage
-                        src={data.img}
-                        alt="Performance icon"
-                        className="~size-16/24"
-                      />
-                      <h4 className="text-brand-white-100 font-matter font-bold text-xl md:text-2xl max-w-xs">
-                        {data.head}
-                      </h4>
-                      <p className="~text-sm/lg text-brand-white-400 font-matter">
-                        {data.para}
-                      </p>
-                    </div>
-                  );
-                })}
-              </div>
+						<div className='flex gap-20 md:flex-row flex-col gap-10 items-center md:items-start'>
+							<div className='grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-10 max-w-2xl mx-auto'>
+								{performanceData.map((data, index) => {
+									const res = index === 2 && windowWidth > 640 ? "col-span-2" : "";
+									return (
+										<div key={index} className={`flex flex-col gap-3 sm:gap-5 ${res}`}>
+											<BaseUrlImage src={data.img} alt='Performance icon' className='~size-16/24' />
+											<h4 className='text-brand-white-100 font-matter font-bold text-xl md:text-2xl max-w-xs'>{data.head}</h4>
+											<p className='~text-sm/lg text-brand-white-400 font-matter'>{data.para}</p>
+										</div>
+									);
+								})}
+							</div>
 
-              <div className="max-w-xl max-h-full">
-                <BaseUrlImage
-                  src="img/performance.png"
-                  alt="Performance diagram"
-                  className="w-full"
-                />
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-    </BrowserOnly>
-  );
+							<div className='max-w-xl max-h-full'>
+								<BaseUrlImage src='img/performance.png' alt='Performance diagram' className='w-full' />
+							</div>
+						</div>
+					</div>
+				</div>
+			)}
+		</BrowserOnly>
+	);
 }
